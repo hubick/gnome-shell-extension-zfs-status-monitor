798075c2a61df78be4003e2dee15b88cad452ea4
Chris Hubick <chris@hubick.com>
2021-10-23 19:48:29 -0600

    Refactor to single indicator containing multiple pools. Implement add/remove/update pool logic.

--- extension.js
+++ extension.js
@@ -13,15 +13,16 @@ const { GObject, St, Clutter } = imports.gi;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const GLib = imports.gi.GLib;
+const ByteArray = imports.byteArray;
 
 
-const ZFSPoolStatusIndicator = GObject.registerClass(
+const ZFSPoolStatus = GObject.registerClass(
 
-  class ZFSPoolStatusIndicator extends PanelMenu.Button {
+  class ZFSPoolStatus extends St.BoxLayout {
 
-    _init(zfs_pool_name) {
-      super._init(0.0, 'ZFS Pool ' + zfs_pool_name + ' Status Indicator');
-      this._zfs_pool_name = zfs_pool_name;
+    _init(pool_name) {
+      super._init();
+      this._pool_name = pool_name;
 
       const child_labels = new St.BoxLayout();
 
@@ -33,7 +34,7 @@ const ZFSPoolStatusIndicator = GObject.registerClass(
 
       this._pool_name_label = new St.Label({
         'y_align': Clutter.ActorAlign.CENTER,
-        'text': zfs_pool_name,
+        'text': this._pool_name,
         'style_class': 'zfs_pool_unavail'
       });
       child_labels.add_child(this._pool_name_label);
@@ -46,21 +47,28 @@ const ZFSPoolStatusIndicator = GObject.registerClass(
 
       this.add_child(child_labels);
 
-      this.update();
+      this.update_pool_status();
 
       return;
     }
 
+    get_pool_name() {
+      return this._pool_name;
+    }
+
     get_pool_state() {
       try {
-        let fileContents = GLib.file_get_contents('/proc/spl/kstat/zfs/' + this._zfs_pool_name + '/state');
-        if (fileContents[0] === true) return String(fileContents[1]);
-      } catch (e) { }
+        let fileContents = GLib.file_get_contents('/proc/spl/kstat/zfs/' + this._pool_name + '/state');
+        if (fileContents[0] === true) return ByteArray.toString(fileContents[1]);
+      } catch (e) {
+        // logError(e);
+      }
       return 'UNAVAIL';
     }
 
-    update() {
+    update_pool_status() {
       const current_pool_state = this.get_pool_state();
+      log('Current Pool State: ' + current_pool_state);
       if (current_pool_state != this._previous_pool_state) {
         this._pool_name_label.style_class = 'zfs_pool_' + current_pool_state.toLowerCase();
         this._previous_pool_state = current_pool_state;
@@ -68,9 +76,51 @@ const ZFSPoolStatusIndicator = GObject.registerClass(
       return;
     }
 
-  } // class ZFSPoolStatusIndicator
+  } // class ZFSPoolStatus
 
-); // const ZFSPoolStatusIndicator
+); // const ZFSPoolStatus
+
+
+const ZFSStatusIndicator = GObject.registerClass(
+
+  class ZFSStatusIndicator extends PanelMenu.Button {
+
+    _init() {
+      super._init(0.0, 'ZFS Status Monitor');
+
+      this._pools = new St.BoxLayout();
+      this.add_child(this._pools);
+
+      return;
+    }
+
+    add_pool(pool_name) {
+      const new_pool = new ZFSPoolStatus(pool_name);
+      this._pools.add_child(new_pool); //TODO Keep these sorted by pool_name?
+      return new_pool;
+    }
+
+    get_pool(pool_name) {
+      for (const pool of this._pools) {
+        if (pool.get_pool_name() == pool_name) {
+          return pool;
+        }
+      }
+      return null;
+    }
+
+    remove_pool(pool_name) {
+      const removed_pool = this.get_pool(pool_name);
+      if (removed_pool) {
+        this._pools.remove_child(removed_pool);
+        removed_pool.destroy();
+      }
+      return removed_pool;
+    }
+
+  } // class ZFSStatusIndicator
+
+); // const ZFSStatusIndicator
 
 
 class ZFSStatusMonitorExtension {
@@ -81,14 +131,50 @@ class ZFSStatusMonitorExtension {
   }
 
   enable() {
-    this._zfs_pool_status_indicator = new ZFSPoolStatusIndicator('MyZFSPool');
-    Main.panel.addToStatusArea(this._uuid, this._zfs_pool_status_indicator);
+    this._status_indicator = new ZFSStatusIndicator();
+    this._previous_pool_names = [];
+    this.update_pools();
+    Main.panel.addToStatusArea(this._uuid, this._status_indicator);
+    return;
+  }
+
+  get_pool_names() {
+    return ['MyZFSPool'];
+  }
+
+  update_pools() {
+    const current_pool_names = this.get_pool_names();
+
+    // Remove pools that no longer exist.
+    for (const previous_pool_name of this._previous_pool_names) {
+      if (current_pool_names.indexOf(previous_pool_name) <= -1) {
+        this._status_indicator.remove_pool(previous_pool_name);
+      }
+    }
+
+    // Update any pools that did exist previously.
+    for (const current_pool_name of current_pool_names) {
+      if (this._previous_pool_names.indexOf(current_pool_name) >= 0) {
+        this._status_indicator.get_pool(current_pool_name).update_pool_status();
+      }
+    }
+
+    // Add any pools that didn't exist previously.
+    for (const current_pool_name of current_pool_names) {
+      if (this._previous_pool_names.indexOf(current_pool_name) <= -1) {
+        this._status_indicator.add_pool(current_pool_name);
+      }
+    }
+
+    this._previous_pool_names = current_pool_names;
+
     return;
   }
 
   disable() {
-    this._zfs_pool_status_indicator.destroy();
-    this._zfs_pool_status_indicator = null;
+    this._status_indicator.destroy();
+    this._status_indicator = null;
+    this._previous_pool_names = [];
     return;
   }
 
